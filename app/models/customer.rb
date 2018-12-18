7c3b2a52487b5acf35feb0c4ee32460db1df168a
marcusplenty <marcusplenty@gmail.com>
2018-12-18 10:33:54 -0500

    Completed Challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,61 @@
 class Customer
   attr_accessor :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all_names
+    Customer.all.map do |customer|
+      customer.first_name + " " + customer.last_name
+    end
+  end
+
+  def self.find_all_by_first_name(first_name)
+    Customer.all.select do |customer|
+      customer.first_name == first_name
+    end
+  end
+
+  def self.find_by_name(name)
+    name_parts= name.split(" ")
+    Customer.all.find do |customer|
+      customer.first_name == name_parts[0]
+      customer.last_name == name_parts[1]
+    end
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self,restaurant,content,rating)
+  end
+
+  def num_reviews
+    Review.all.count do |review|
+      review.customer == self
+    end
+  end
+
+  def my_reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def restaurants
+    my_reviews.map do |review|
+      review.restaurant
+    end.uniq
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,5 @@
 require_relative '../config/environment.rb'
+require_rel '../app'
 
 def reload
   load 'config/environment.rb'
@@ -6,6 +7,19 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+c1 = Customer.new("Marcus", "Plenty")
+c2 = Customer.new("Kenya", "Plenty")
+c3 = Customer.new("Marcus", "Jackson")
+
+r1 = Restaurant.new("Hooters")
+r2 = Restaurant.new("Burger King")
+
+rw1 = Review.new(c1,r1,9,"Awesome")
+rw2 = Review.new(c1,r2,10,"Bad")
+rw3 = Review.new(c2,r2,5,"Worse")
+rw4 = Review.new(c1,r1,4,"Eh")
+rw5 = Review.new(c3,r2,10,"Bad")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
