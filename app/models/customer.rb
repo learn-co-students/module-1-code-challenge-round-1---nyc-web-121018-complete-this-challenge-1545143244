8c74aca054d6dff6e561967c2fa9eb23f1e56add
github username <github email address>
2018-12-18 10:36:40 -0500

    dania

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,65 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all =[]
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def restaurants
+    self.reviews.map do |review|
+      review.restaurant
+    end
+  end
+
+  def self.find_by_name(name)
+     self.all.find {|customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.find do |customer|
+      found = self.all.find {|customer| customer.full_name.split[0] == name}
+    end
+  end
+
+  def self.all_names
+    self.all.map {|customer| customer.full_name}
+  end
+
+  def add_review(restaurant, content, rating) #restaurant, customer, content, rating
+    Review.new(restaurant, self, content, rating)
+  end
+
+  # def num_reviews
+  #   reviews.customer == self
+  # end
+
+  # def average_star_rating
+  #   #find average star rating of restaurant
+  # end
+  #
+
+
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,22 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+
+#Restaurants (name)
+  lpq = Restaurant.new("Le Pain Quotidian")
+  tb = Restaurant.new("Taco Bell")
+  bww = Restaurant.new("Buffalo Wild Wings")
+
+#Customer (first_name, last_name)
+  ari = Customer.new("arianna", "geordiadis")
+  adam = Customer.new("adam", "ignotofsky")
+  keyla = Customer.new("keyla", "payano")
+
+#Reviews (restaurant, customer, content, rating)
+  r1 = Review.new(lpq, ari, "I love the avo toast", 3)
+  r2 = Review.new(tb, adam, "It's my drunk food go to", 5)
+  r3 = Review.new(bww, keyla, "You gotta do Wing Tuesday!", 4)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
