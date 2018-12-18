eb0271cdf08d652ac706815efb6849d8c151684e
github username <github email address>
2018-12-18 10:38:00 -0500

    hi, this was great\!\!

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+@@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
@@ -9,4 +11,19 @@ class Customer
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def Customer.all ##########
+    @@all << self
+  end
+
+def Customer.find_by_name(name) ############
+self.full_name.find do |name|
+  full_name == name
+  end
 end
+
+def Customer.all_names ###################
+    array = self.all.each do |array|
+      array << []
+    end
+  end
