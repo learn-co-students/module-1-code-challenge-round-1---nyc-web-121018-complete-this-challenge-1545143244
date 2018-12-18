856d1561c63d2b5ac55e4b88b3cc9855e35925e3
pirainogi <pirainogi@gmail.com>
2018-12-18 10:35:28 -0500

    finished

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,36 @@
 class Review
-  
-end
 
+  attr_reader :customer, :restaurant, :content, :rating
+  @@all = []
+
+  def self.all
+    @@all
+  end
+                  # restaurant, content, rating
+  def initialize(customer, restaurant, content, rating)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @rating = rating
+
+    @@all << self
+  end
+
+  #THE BELOW ALL WORK BECAUSE THEY ARE SET AS ATTR-READER MACROS 
+  # def customer
+  #   @customer
+  # end
+  #
+  # def restaurant
+  #   @restaurant
+  # end
+  #
+  # def rating
+  #   @rating
+  # end
+  #
+  # def content
+  #   @content
+  # end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,27 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+                  #first_name, last_name
+c1 = Customer.new("Bob", "Smith")
+c2 = Customer.new("Jane", "Smith")
+c3 = Customer.new("Eric", "Ball")
+c4 = Customer.new("Lyra", "Quinn")
+
+                    #name
+r1 = Restaurant.new("mcs")
+r2 = Restaurant.new("bk")
+r3 = Restaurant.new("wendys")
+r4 = Restaurant.new("arbys")
+r5 = Restaurant.new("chickfila")
+
+                #customer, restaurant, content, rating
+rv1 = Review.new(c1, r1, "good", 4)
+rv2 = Review.new(c2, r2, "bad", 1)
+rv3 = Review.new(c2, r3, "decent", 3)
+rv4 = Review.new(c3, r1, "great", 4)
+rv5 = Review.new(c4, r1, "amazong", 5)
+rv6 = Review.new(c4, r4, "okay", 2)
+rv7 = Review.new(c4, r5, "horrible", 0)
+ 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
