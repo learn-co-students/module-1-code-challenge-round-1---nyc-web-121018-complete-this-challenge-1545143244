d55c4a41da61fe2f830c40c317ff099339c495ee
github username <github email address>
2018-12-18 10:42:33 -0500

    finished

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,66 @@
+# **JOINER TABLER **
 class Review
-  
-end
 
+  attr_reader :customer, :restaurant, :rating, :content
+
+  @@all = []
+
+
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  # - `Review.all`
+  #   - returns all of the reviews
+
+  def self.customer
+
+  end
+  # - `Review#customer`
+  #   - returns the customer object for that given review
+  #   # - Once a review is created, I should not be able to change the author
+
+
+  def self.restaurant
+
+  end
+  # - `Review#restaurant`
+  #   - returns the restaurant object for that given review
+  #   - Once a review is created, I should not be able to change the restaurant
+
+  def self.rating
+    "I give this restaurant #{fixnum} stars"
+  end
+  # - `Review#rating`
+  #   - returns the star rating for a restaurant. This should be an integer from 1-5
+
+  def self.content
+    "#{content}"
+  end
+  # - `Review#content`
+  #   - returns the review content, as a string, for a particular review
+
+
+end
+#end of review class
+
+
+# - `Review.all`
+#   - returns all of the reviews
+# - `Review#customer`
+#   - returns the customer object for that given review
+#   - Once a review is created, I should not be able to change the author
+# - `Review#restaurant`
+#   - returns the restaurant object for that given review
+#   - Once a review is created, I should not be able to change the restaurant
+# - `Review#rating`
+#   - returns the star rating for a restaurant. This should be an integer from 1-5
+# - `Review#content`
+#   - returns the review content, as a string, for a particular review
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,33 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+c1 = Customer.new("Mary", "Peterson")
+c2 = Customer.new("John", "Smith")
+c3 = Customer.new("John", "Jones")
+c4 = Customer.new("Terry", "Kim")
+c5 = Customer.new("Emily", "Rells")
+
+r1 = Restaurant.new("McDonalds")
+r2 = Restaurant.new("Wendys")
+r3 = Restaurant.new("Burger King")
+
+re1 = Review.new(c1, r2, 4, "It was OK")
+re2 = Review.new(c2, r2, 8, "I love this restuarant")
+re3 = Review.new(c3, r2, 7, "It was fun")
+re4 = Review.new(c4, r2, 7, "Cool place")
+re5 = Review.new(c1, r2, 2, "I hated it")
+re6 = Review.new(c5, r2, 9, "I loved my waiter")
+re7 = Review.new(c4, r2, 2, "Nothing was good")
+re8 = Review.new(c5, r2, 9, "Would eat here again")
+re9 = Review.new(c3, r2, 10, "Highly recommend")
+
+
+
+
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
