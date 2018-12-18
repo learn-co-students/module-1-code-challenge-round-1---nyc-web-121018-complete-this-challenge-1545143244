6c95d8196f0cbf4dd0cce2b7890a162fc9d9d7d8
github username <github email address>
2018-12-18 10:34:20 -0500

    code challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,72 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+
+   @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(full_name)
+    self.all.select do |customer|
+      customer.full_name == full_name
+
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select do |customer|
+      customer.first_name == name
+    end
+  end
+
+  def add_review(restaurant, rating=0, content)
+    Review.new(restaurant, self, rating=0, content)
+  end
+
+  def num_reviews
+    Review.all.select do |reviews|
+      reviews.customer == self
+
+    end
+  end
+
+  def restaurants
+    Review.all.map do |reviews|
+      reviews.restaurant
+    end
+  end
+
+
+
+
+
+
 end
+
+
+#- `Customer.all`
+  #- should return **all** of the customer instances
+#- `Customer.find_by_name(name)`
+  #- given a string of a **full name**, returns the **first customer** whose full name matches
+#- `Customer.find_all_by_first_name(name)`
+  #- given a string of a first name, returns an **array** containing all customers with that first name
+#  - should return an **array** of all of the customer full names
+
+#- `Customer#add_review(restaurant, content, rating)`
+  #- given a **restaurant object**, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+#- `Customer#num_reviews`
+  #- Returns the total number of reviews that a customer has authored
+#- `Customer#restaurants`
+  #- Returns a **unique** array of all restaurants a customer has reviewed
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,53 @@
 class Review
-  
+  attr_reader :restaurant, :customer, :rating, :content
+
+  @@all = []
+
+  def initialize(restaurant, customer, rating=0, content)
+    @restaurant = restaurant
+    @customer = customer
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    @customer
+  end
+
+  def restaurant
+    @restaurant
+  end
+
+  def rating
+    @rating
+  end
+
+  def content
+    @content
+  end
+
 end
 
+
+
+
+
+
+
+#- `Review.all`
+  #- returns all of the reviews
+#- `Review#customer`
+  #- returns the customer object for that given review
+  #- Once a review is created, I should not be able to change the author
+#- `Review#restaurant`
+  #- returns the restaurant object for that given review
+  #- Once a review is created, I should not be able to change the restaurant
+#- `Review#rating`
+  #- returns the star rating for a restaurant. This should be an integer from 1-5
+#- `Review#content`
+  #- returns the review content, as a string, for a particular review
